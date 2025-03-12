7fc8b436c843c8fdfb0ce527578003a557a73173
MeetGangani <meetgangani56@gmail.com>
2025-03-12 13:36:04 +0530

    student details

--- models/userModel.js
+++ models/userModel.js
@@ -31,6 +31,81 @@ const userSchema = mongoose.Schema(
       },
       default: 'student'
     },
+    // Common fields for contact info
+    contactNumber: {
+      type: String,
+      validate: {
+        validator: function(v) {
+          return /^\+?[\d\s-]{10,}$/.test(v);
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      }
+    },
+    address: {
+      street: String,
+      city: String,
+      state: String,
+      pincode: String,
+      country: String
+    },
+    // Student specific fields
+    gender: {
+      type: String,
+      enum: {
+        values: ['male', 'female', 'other'],
+        message: '{VALUE} is not a valid gender'
+      },
+      required: function() { return this.userType === 'student'; }
+    },
+    dateOfBirth: {
+      type: Date,
+      required: function() { return this.userType === 'student'; }
+    },
+    guardianInfo: {
+      name: {
+        type: String,
+        required: function() { return this.userType === 'student'; }
+      },
+      contactNumber: {
+        type: String,
+        required: function() { return this.userType === 'student'; },
+        validate: {
+          validator: function(v) {
+            return /^\+?[\d\s-]{10,}$/.test(v);
+          },
+          message: props => `${props.value} is not a valid phone number!`
+        }
+      },
+      relation: {
+        type: String,
+        required: function() { return this.userType === 'student'; }
+      }
+    },
+    // Institute specific fields
+    website: {
+      type: String,
+      required: function() { return this.userType === 'institute'; },
+      validate: {
+        validator: function(v) {
+          return /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/.test(v);
+        },
+        message: props => `${props.value} is not a valid URL!`
+      }
+    },
+    registrationNumber: {
+      type: String,
+      required: function() { return this.userType === 'institute'; },
+      unique: true,
+      sparse: true
+    },
+    instituteType: {
+      type: String,
+      enum: {
+        values: ['school', 'college', 'university'],
+        message: '{VALUE} is not a valid institute type'
+      },
+      required: function() { return this.userType === 'institute'; }
+    },
     lastLogin: {
       type: Date,
       default: Date.now
